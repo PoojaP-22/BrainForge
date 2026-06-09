5807d8c1897609be61a396e6231f0aaa184f85bd
Pooja <poojapjan22@gmail.com>
2026-06-09 22:44:59 +0530

    Implement Global exception handling and updated CourseService with ResourceNotFoundException

--- src/main/java/com/courseplatform/courseplatform_backend/service/CourseService.java
+++ src/main/java/com/courseplatform/courseplatform_backend/service/CourseService.java
@@ -1,5 +1,6 @@
 package com.courseplatform.courseplatform_backend.service;
 import com.courseplatform.courseplatform_backend.entity.Course;
+import com.courseplatform.courseplatform_backend.exception.ResourceNotFoundException;
 import com.courseplatform.courseplatform_backend.repository.CourseRepository;
 import org.springframework.stereotype.Service;
 
@@ -11,35 +12,56 @@ public class CourseService {
     private final CourseRepository repository;
 
     public CourseService(CourseRepository repository) {
+
         this.repository = repository;
     }
 
     public List<Course> getAllCourses() {
+
         return repository.findAll();
     }
 
     public Course getCourseById(Long id) {
-        return repository.findById(id).orElse(null);
+        return repository.findById(id)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException(
+                            "Course",
+                            "id",
+                            id
+        ));
     }
 
     public Course addCourse(Course course) {
+
         return repository.save(course);
     }
 
     public Course updateCourse(Long id, Course course) {
-        Course existing = repository.findById(id).orElse(null);
-
-        if (existing != null) {
+        Course existing = repository.findById(id)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException(
+                                "Course",
+                                "id",
+                                id
+                        ));
             existing.setTitle(course.getTitle());
             existing.setInstructor(course.getInstructor());
             existing.setPrice(course.getPrice());
-            return repository.save(existing);
-        }
 
-        return null;
+            return repository.save(existing);
+
     }
 
     public void deleteCourse(Long id) {
-        repository.deleteById(id);
+
+        Course existing = repository.findById(id)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException(
+                                "Course",
+                                "id",
+                                id
+                        ));
+
+        repository.delete(existing);
     }
 }
